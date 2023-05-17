09eb0727772a5ed88370fedbf5d41d220790d62f
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-17 13:15:43 +0100

    add button like

--- main.js
+++ main.js
@@ -56,8 +56,8 @@ const posts = [
     }
 ];
 
-const container = document.getElementById('container');
 
+const container = document.getElementById('container');
 
 posts.forEach(element => {
 
@@ -81,7 +81,7 @@ posts.forEach(element => {
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#!" data-postid="1">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
@@ -96,3 +96,17 @@ posts.forEach(element => {
 });
 
 
+const button = document.querySelectorAll('.js-like-button');
+const count = document.querySelectorAll('.js-likes-counter');
+
+
+// button.addEventListener('click',
+//     console.log('ciao a tutti')
+// )
+
+button.forEach((element, i) => {
+    element.addEventListener('click', function () {
+        posts[i].likes++;
+        count[i].innerHTML = posts[i].likes;
+    })
+})
